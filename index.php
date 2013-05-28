7cc006593db72a03d9dcf2d7cc68f01ee3dbe221
Rémi Chanaud <remi.chanaud@gmail.com>
2013-05-28 14:54:04 +0200

    La map, basique

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>DataViz Project</title>
+
+	<!-- Style -->
+	<link rel="stylesheet" href="css/main.css">
+	<!-- /Style -->
+
+</head>
+<body>
+	
+	<!-- Map -->
+	<div id="map"></div>
+	<div id="clicked-state"></div>
+	<!-- /Map -->
+
+	<!-- Scripts -->
+
+		<!-- Dépendances de la map -->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
+		<script src="js/raphael-min.js"  type="text/javascript"></script>
+		<script src="js/jquery.usmap.js" type="text/javascript"></script>
+		<!-- Dépendances de la map -->
+
+		<script src="js/main.js" type="text/javascript"></script>
+
+	<!-- /Scripts -->
+	
+</body>
+</html>
